dbce23b90b674fde6e897cd61e57fdf1007bddea
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2019-05-26 19:00:43 +0300

    Omega-Statistics project initial commit

---
+++ Omega-Statistic/SSE/cmake-build-debug/CMakeFiles/SSE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SSE.dir/main.c.o"
+  "SSE.pdb"
+  "SSE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SSE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Omega-Statistic/Serial/cmake-build-debug/CMakeFiles/Serial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Serial.dir/main.c.o"
+  "Serial.pdb"
+  "Serial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Serial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
