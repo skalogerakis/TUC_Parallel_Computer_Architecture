ad7fea4b1db36eff267909c6c3211eee79951c8a
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2019-05-27 03:13:15 +0300

    SSE done.Bonus part left

--- Omega-Statistic/SSE/main.c
+++ Omega-Statistic/SSE/main.c
@@ -4,6 +4,8 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <float.h>
+//ADDED INCLUDE FOR SSE
+#include <xmmintrin.h>
 #define MINSNPS_B 5
 #define MAXSNPS_E 20
 
@@ -26,6 +28,20 @@ float randpval (void)
     return r;
 }
 
+float maxCalc(float x, float y){
+    if(x > y){
+        return x;
+    }
+    return y;
+}
+
+float minCalc(float x, float y){
+    if(x < y){
+        return x;
+    }
+    return y;
+}
+
 int main(int argc, char ** argv)
 {
     assert(argc==2);
@@ -39,23 +55,24 @@ int main(int argc, char ** argv)
 
     srand(1);
 
-    float * mVec = (float*)malloc(sizeof(float)*N);
+    float * mVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(mVec!=NULL);
 
-    float * nVec = (float*)malloc(sizeof(float)*N);
+    float * nVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(nVec!=NULL);
 
-    float * LVec = (float*)malloc(sizeof(float)*N);
+    float * LVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(LVec!=NULL);
 
-    float * RVec = (float*)malloc(sizeof(float)*N);
+    float * RVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(RVec!=NULL);
 
-    float * CVec = (float*)malloc(sizeof(float)*N);
+    float * CVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(CVec!=NULL);
 
-    float * FVec = (float*)malloc(sizeof(float)*N);
+    float * FVec = (float*)_mm_malloc(sizeof(float)*N,16);
     assert(FVec!=NULL);
+
 
     for(unsigned int i=0;i<N;i++)
     {
@@ -73,27 +90,138 @@ int main(int argc, char ** argv)
         assert(RVec[i]>=0.0f && RVec[i]<=1.0f*nVec[i]);
         assert(CVec[i]>=0.0f && CVec[i]<=1.0f*mVec[i]*nVec[i]);
     }
+
+    /*
+     * SSE CHANGES
+     */
+
+    //Implementation with pointers.
+
+    __m128 *mVec_ptr = (__m128 *) mVec;
+    __m128 *nVec_ptr = (__m128 *) nVec;
+    __m128 *LVec_ptr = (__m128 *) LVec;
+    __m128 *RVec_ptr = (__m128 *) RVec;
+    __m128 *CVec_ptr = (__m128 *) CVec;
+    __m128 *FVec_ptr = (__m128 *) FVec;
+    __m128 maxF_vec = _mm_setzero_ps();
+    __m128 avgF_vec = _mm_setzero_ps();
+    __m128 minF_vec = _mm_set_ps1(FLT_MAX); //TODO CHANGE THAT
+    //__m128 *maxv__m128 = (__m128 *) maxv;
+
+
+
+    __m128 temp_num_0;
+    __m128 temp_num_1;
+    __m128 temp_num_2;
+    __m128 temp_num;
+    __m128 temp_den_0;
+    __m128 temp_den_1;
+    __m128 temp_den;
+
+    __m128 vec_num;
+    __m128 vec_num_0;
+    __m128 vec_num_1;
+    __m128 vec_num_2;
+    __m128 vec_den;
+    __m128 vec_den_0;
+    __m128 vec_den_1;
+
+    /*
+     * We prefer to declare the follow const values as __m128 and
+     * not as row data as sse handles better them that way
+     */
+    const __m128 temp_one = _mm_set1_ps(1.0f);
+    const __m128 __temp_one = _mm_set1_ps(0.01f);
+    const __m128 temp_two = _mm_set1_ps(2.0f);
+
     double timeOmegaTotalStart = gettime();
     for(unsigned int j=0;j<iters;j++)
     {
         avgF = 0.0f;
         maxF = 0.0f;
         minF = FLT_MAX;
-        for(unsigned int i=0;i<N;i++)
+        for(unsigned int i=0; i<N/4 ;i++)
         {
-            float num_0 = LVec[i]+RVec[i];
-            float num_1 = mVec[i]*(mVec[i]-1.0f)/2.0f;
-            float num_2 = nVec[i]*(nVec[i]-1.0f)/2.0f;
-            float num = num_0/(num_1+num_2);
-            float den_0 = CVec[i]-LVec[i]-RVec[i];
-            float den_1 = mVec[i]*nVec[i];
-            float den = den_0/den_1;
+            //float num_0 = LVec[i] + RVec[i];
+            temp_num_0 = _mm_add_ps(LVec_ptr[i],RVec_ptr[i]);
 
-            FVec[i] = num/(den+0.01f);
-            maxF = FVec[i]>maxF?FVec[i]:maxF;
-            minF = FVec[i]<minF?FVec[i]:minF;
-            avgF += FVec[i];
+            //float num_1 = mVec[i]*(mVec[i]-1.0f)/2.0f;
+            temp_num_1 = _mm_sub_ps(mVec_ptr[i], temp_one);
+//            temp_num_1 = _mm_div_ps(temp_num_1, twov);
+//            temp_num_1 = _mm_mul_ps(temp_num_1, mVec_ptr[i]);
+            temp_num_1 = _mm_mul_ps(mVec_ptr[i], temp_num_1);
+            temp_num_1 = _mm_div_ps(temp_num_1, temp_two);
+
+
+            //float num_2 = nVec[i]*(nVec[i]-1.0f)/2.0f;
+            temp_num_2 = _mm_sub_ps(nVec_ptr[i], temp_one);
+//            temp_num_2 = _mm_div_ps(temp_num_2, twov);
+//            temp_num_2 = _mm_mul_ps(temp_num_2, nVec_ptr[i]);
+            temp_num_2 = _mm_mul_ps(nVec_ptr[i], temp_num_2);
+            temp_num_2 = _mm_div_ps(temp_num_2, temp_two);
+
+            //float num = num_0/(num_1+num_2);
+            temp_num = _mm_add_ps(temp_num_1,temp_num_2);
+            temp_num = _mm_div_ps(temp_num_0, temp_num);
+
+            //float den_0 = CVec[i]-LVec[i]-RVec[i];
+            temp_den_0 = _mm_sub_ps(CVec_ptr[i], LVec_ptr[i]);
+            temp_den_0 = _mm_sub_ps(temp_den_0, RVec_ptr[i]);
+
+            //float den_1 = mVec[i]*nVec[i];
+            temp_den_1 = _mm_mul_ps(mVec_ptr[i], nVec_ptr[i]);
+
+            //float den = den_0/den_1;
+            temp_den = _mm_div_ps(temp_den_0, temp_den_1);
+
+            //FVec[i] = num/(den+0.01f);
+            FVec_ptr[i] = _mm_add_ps(temp_den, __temp_one);
+            FVec_ptr[i] = _mm_div_ps(temp_num, FVec_ptr[i]);
+
+            //maxF = FVec[i]>maxF?FVec[i]:maxF;
+            maxF_vec = _mm_max_ps(FVec_ptr[i], maxF_vec);
+            minF_vec = _mm_min_ps(FVec_ptr[i], minF_vec);
+            avgF_vec = _mm_add_ps(avgF_vec, FVec_ptr[i]);
+            //minF = FVec[i]<minF?FVec[i]:minF;
+            //avgF += FVec[i];
         }
+
+        //_mm_store_ps(max, _maxF);
+        //_mm_store_ps(max, maxFV);
+        maxF = maxF_vec[0];
+        maxF = maxCalc(maxF_vec[1],maxF);
+        maxF = maxCalc(maxF_vec[2],maxF);
+        maxF = maxCalc(maxF_vec[3],maxF);
+//        maxF = maxF_vec[1]>maxF?maxF_vec[1]:maxF;
+//        maxF = maxF_vec[2]>maxF?maxF_vec[2]:maxF;
+//        maxF = maxF_vec[3]>maxF?maxF_vec[3]:maxF;
+
+        minF = minF_vec[0];
+        minF = minCalc(minF_vec[1],minF);
+        minF = minCalc(minF_vec[2],minF);
+        minF = minCalc(minF_vec[3],minF);
+//        minF = minF_vec[1]<minF?minF_vec[1]:minF;
+//        minF = minF_vec[2]<minF?minF_vec[2]:minF;
+//        minF = minF_vec[3]<minF?minF_vec[3]:minF;
+
+        avgF = avgF_vec[0]+avgF_vec[1]+avgF_vec[2]+avgF_vec[3];
+
+        for (int j = (N - N % 4); j < N; j++) {
+            // Scalar computation of whatever its left
+            float num_0 = LVec[j] + RVec[j];
+            float num_1 = mVec[j] * (mVec[j] - 1.0f) / 2.0f;
+            float num_2 = nVec[j] * (nVec[j] - 1.0f) / 2.0f;
+            float num = num_0 / (num_1 + num_2);
+            float den_0 = CVec[j] - LVec[j] - RVec[j];
+            float den_1 = mVec[j] * nVec[j];
+            float den = den_0 / den_1;
+
+            FVec[j] = num / (den + 0.01f);
+            maxF = FVec[j] > maxF ? FVec[j] : maxF;
+            minF = FVec[j]<minF?FVec[j]:minF;
+            avgF += FVec[j];
+        }
+
     }
     double timeOmegaTotal = gettime()-timeOmegaTotalStart;
     double timeTotalMainStop = gettime();
