cdd629630af28438b9c70614a7301816abe513ef
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2019-05-28 01:12:35 +0300

    Started MPI implementation

---
+++ Omega-Statistic/SEE-PTHREADS-MPI/cmake-build-debug/CMakeFiles/SEE_PTHREADS_MPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SEE_PTHREADS_MPI.dir/main.c.o"
+  "SEE_PTHREADS_MPI.pdb"
+  "SEE_PTHREADS_MPI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SEE_PTHREADS_MPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
